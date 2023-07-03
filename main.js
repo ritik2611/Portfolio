83bcf3d5a21f6122383f54b52175b8c3c6b70b36
Ritik Kumar Pandey <ritikpandey2610@gmail.com>
2023-07-03 19:52:21 +0530

    temperature convertor

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const cursorSpan = document.querySelector(".cursor");
 const textArray = ["Native Android Developer", "Web Designer", "A programmer", "JISCE Student"];
 const typingDelay = 200;
 const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
+const newTextDelay = 1000; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
 
