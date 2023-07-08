1a64d72b241a442b688f14e6ef2c2db9a7deaa86
Sand Man <rajeshbhadra62@gmail.com>
2023-07-08 15:37:35 +0530

    Backend is done �

--- temp.js
+++ temp.js
@@ -2,5 +2,9 @@ function getCelcius(){
     const temp_F = document.querySelector('input').value;
     document.querySelector('input').textContent
    const c = (temp_F - 32) * (5/9);
-   console.log(c + " Celcius");    
+   console.log(c + " Celcius"); 
+   var temp = document.getElementById("temp-c");
+   temp.textContent = parseInt(c)  + "°C";
 }
+
+
